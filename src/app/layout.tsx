65e8ccb9671841c686f1a0ef16bc502b27edc018
BiscoteBriochet <203165427+BiscoteBriochet@users.noreply.github.com>
2025-06-27 17:18:09 +0200

    Update layout.tsx
    CARBONARA -> Creep'Mobilités

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import { mantineHtmlProps, MantineProvider } from "@mantine/core";
 
 export const metadata: Metadata = {
-    title: "Carbonara",
-    description: "Comprehensive And Rapid Browser for Organized Navigation And Route Assistance: A P.E.S.T.O. Train Router"
+    title: "Creep'Mobilités",
+    description: "Un calculateur d'itinéraire pour vous déplacer dans la magnifique ville de Paris"
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
