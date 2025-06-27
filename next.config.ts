54c35831859c23d29f92366770c4927fe9a0e815
BiscoteBriochet <203165427+BiscoteBriochet@users.noreply.github.com>
2025-06-27 22:18:47 +0200

    Update next.config.ts
    carbonara -> creepmobilites

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: "/carbonara",
+    basePath: "/creepmobilites",
     output: "export",
     reactStrictMode: true,
     experimental: {
