9aca07102791ce7943980445a846f3f3106315ab
BiscoteBriochet <203165427+BiscoteBriochet@users.noreply.github.com>
2025-06-27 19:02:28 +0200

    Update networkData.ts

--- src/app/lib/networkData.ts
+++ src/app/lib/networkData.ts
@@ -3,16 +3,70 @@ import { NetworkData } from "@/app/lib/interfaces";
 const networkData = {
     lines: [
         {
-            id: "1",
-            name: "Métro 1",
-            colour: "#b80b4b",
+            id: "M1",
+            name: "",
+            colour: "#ffcd02",
+            type: "LSR"
+        },{
+            id: "M2",
+            name: "",
+            colour: "#006db8",
+            type: "LSR"
+        },{
+            id: "M3",
+            name: "",
+            colour: "#9b993b",
+            type: "LSR"
+        },{
+            id: "M4",
+            name: "",
+            colour: "#bb4a9b",
+            type: "LSR"
+        },{
+            id: "M5",
+            name: "",
+            colour: "#f78f4b",
+            type: "LSR"
+        },{
+            id: "M6",
+            name: "",
+            colour: "#77c696",
+            type: "LSR"
+        },{
+            id: "M7",
+            name: "",
+            colour: "#f59fb3",
             type: "LSR"
         },
         {
-            id: "A",
-            name: "RER A",
-            colour: "#cec92a",
-            type: "HSR"
+            id: "RER A",
+            name: "",
+            colour: "#ed1c2a",
+            type: "LSR"
+        },
+        {
+            id: "RER B",
+            name: "",
+            colour: "#4c90cd",
+            type: "LSR"
+        },
+        {
+            id: "RER C",
+            name: "",
+            colour: "#ffcd02",
+            type: "LSR"
+        },
+        {
+            id: "RER D",
+            name: "",
+            colour: "#008c5a",
+            type: "LSR"
+        },
+        {
+            id: "RER E",
+            name: "",
+            colour: "#bb4a9b",
+            type: "LSR"
         }
     ],
     stations: [
